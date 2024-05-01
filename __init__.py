4bc5b2e330b75415c497f051b043ae7723cea28c
yuracoff18 <yurayt18@gmail.com>
2024-05-01 11:11:35 -0500

    multiplicacion

--- __init__.py
+++ __init__.py
@@ -1,16 +1,19 @@
 from addition import add
+from multiplication import mul
 
 def game():
      score = 0
 
      while True:
          print('======== Menu ========'
+         '\n2. Multiplication'
          '\n1. Add'
          '\n0. Exit')
 
          option = int(input('\nChoice an option: '))
 
          if option == 0:
+             print(f"Your total score was {score}")
              break
 
          num_1 = int(input('Enter first number: '))
@@ -21,6 +24,16 @@ def game():
 
          if option == 1:
              result = add(num_1, num_2)
+         elif option == 2:
+             result = mul(num_1, num_2)
+         elif option == 3:
+             pass
+         elif option == 4:
+             pass
+         elif option == 5:
+             pass
+         elif option == 6:
+             pass
 
          if result == answer:
              score += 1
